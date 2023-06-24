203e0b05e5e09523e0d2f59883d6ce411e0af91a
Ben Millwood <thebenmachine+git@gmail.com>
2023-06-24 23:30:05 +0100

    tesseract endpoint

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import json
 import os
 import subprocess
 import sys
+import tempfile
 
+import multipart
 import pytesseract
+import yattag
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     def path_query(self):
@@ -48,8 +51,71 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(b'Restarting...')
             sys.exit(0)
 
+    def parse_multipart_content_type(self):
+        try:
+            content_type, boundary = self.headers['Content-Type'].split(';', maxsplit=1)
+        except ValueError:
+            self.send_error(code=400)
+            return
+
+        if content_type != 'multipart/form-data':
+            self.send_error(code=415)
+            return
+
+        k, v = boundary.split('=', maxsplit=1)
+        if k.strip() != 'boundary':
+            self.send_error(code=400)
+            return
+
+        return v
+
     def tesseract_POST(self):
-        self.send_response(code=501)
+        boundary = self.parse_multipart_content_type()
+        if boundary is None:
+            return
+        mp = multipart.MultipartParser(
+            stream=self.rfile,
+            boundary=boundary,
+            content_length=int(self.headers['Content-Length']),
+        )
+        (image,) = mp.get_all(name='image')
+
+        with tempfile.NamedTemporaryFile() as tmp:
+            tmp.write(image.raw)
+            data = [
+                line.split('\t')
+                for line in pytesseract.image_to_data(tmp.name).splitlines()
+            ]
+
+        self.send_response(code=200)
+        self.send_header(keyword='Content-Type', value='text/html')
+        self.end_headers()
+
+        doc = yattag.Doc()
+        doc.asis('<!DOCTYPE html>\n')
+        with doc.tag('html'):
+            with doc.tag('head'):
+                doc.stag('meta', charset='UTF-8')
+                with doc.tag('title'):
+                    doc.text('Tesseract API')
+                with doc.tag('style', type='text/css'):
+                    doc.text('table, td { border: 1px solid black; }')
+
+            with doc.tag('body'):
+                with doc.tag('table'):
+                    with doc.tag('thead'):
+                        with doc.tag('tr'):
+                            for header in data[0]:
+                                with doc.tag('td'):
+                                    doc.text(header)
+                    with doc.tag('tbody'):
+                        for line in data[1:]:
+                            with doc.tag('tr'):
+                                for field in line:
+                                    with doc.tag('td'):
+                                        doc.text(field)
+
+        self.wfile.write(doc.getvalue().encode('utf-8'))
 
     def uppercase_POST(self):
         if self.headers['Content-Type'] != 'application/json':
@@ -68,7 +134,27 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         self.wfile.write(response)
 
     def tesseract_GET_HEAD(self, only_head: bool):
-        self.send_response(code=404)
+        self.send_response(code=200)
+        self.send_header(keyword='Content-Type', value='text/html')
+        self.end_headers()
+
+        if only_head:
+            return
+
+        doc = yattag.Doc()
+        doc.asis('<!DOCTYPE html>\n')
+        with doc.tag('html'):
+            with doc.tag('head'):
+                doc.stag('meta', charset='UTF-8')
+                with doc.tag('title'):
+                    doc.text('Tesseract API')
+
+            with doc.tag('body'):
+                with doc.tag('form', method='POST', enctype='multipart/form-data'):
+                    doc.stag('input', name='image', type='file')
+                    doc.stag('input', type='submit')
+
+        self.wfile.write(doc.getvalue().encode('utf-8'))
 
     def root_GET_HEAD(self, only_head: bool):
         self.send_response(code=200)
